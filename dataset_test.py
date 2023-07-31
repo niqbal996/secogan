f8516a3a4d2dffac05f294a6e171cf6452505865
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-07-31 12:19:09 +0200

    Testing script to generate data from pretrained models

---
+++ dataset_test.py
@@ -0,0 +1,23 @@
+from torch import utils
+from torchvision import datasets
+import torchvision.transforms as T
+
+
+class Dataset(utils.data.Dataset):
+    def __init__(self, source_path, load_size):
+        tfm = T.Compose([
+            T.Resize((load_size, 2 * load_size)),
+            T.ToTensor(),
+            # T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+
+        self.source = datasets.ImageFolder(root=source_path, transform=tfm)
+        self.size = len(self.source)
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        # 0 means only images
+        return self.source[idx][0]
